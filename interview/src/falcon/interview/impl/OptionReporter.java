31512da28b27c7c8e15acdb3a16dab7841f18d03
greg2018 <526473+gb96@users.noreply.github.com>
2021-04-15 16:54:11 -0400

    The solution for HSBC trader report

--- interview/src/falcon/interview/impl/OptionReporter.java
+++ interview/src/falcon/interview/impl/OptionReporter.java
@@ -6,7 +6,10 @@ import falcon.interview.api.Trade;
 import falcon.interview.api.TradeApi;
 
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 
 /**
@@ -23,49 +26,62 @@ public class OptionReporter {
 
     TradeApi api;
     LinkedBlockingQueue<Trade> queue = new LinkedBlockingQueue<>();
-    int queueDepth = 0;
+    //int queueDepth = 0;
 
     public OptionReporter(TradeApi api) {
         this.api = api;
     }
 
+    AtomicBoolean publisherNotCompleted =new  AtomicBoolean(true);
+
     public void performReporting() {
         List<Instrument> allInstruments = api.getAllInstruments();
+
+        Map<Integer, Instrument> allInstrumentMap = new HashMap<>();
+        for(Instrument instrument : allInstruments){
+            allInstrumentMap.put(instrument.getId(), instrument);
+        }
         new Thread(() -> {
             Trade nextTrade = api.getNextTrade();
             while (nextTrade != null) {
                 queue.add(nextTrade);
-                queueDepth++;
+                //queueDepth++;
                 nextTrade = api.getNextTrade();
+               // System.out.println("++++++Thread01->"+ queueDepth);
+                System.out.println("++++++Thread01->");
             }
+            publisherNotCompleted.set(false);
         }).start();
         new Thread(() -> {
-            while (true) {
-                if (queueDepth == 0) {
-                    try {
-                        Thread.sleep(500L);
-                    } catch (InterruptedException e) {
-                    }
-                } else {
-                    Trade x = queue.poll();
-                    queueDepth--;
-                    double y = api.valueInstrument(allInstruments.get(x.getInstrumentId()));
-                    String z = getName(allInstruments, x);
-                    try {
-                        double v = y * x.getQuantity();
-                        api.reportTradeValue(x.getId(), z, v);
-                    } catch (Throwable t) {
-                        api.reportErrorTrade(x.getId());
-                    }
-                    api.allTradesReported();
+            while (publisherNotCompleted.get() || !queue.isEmpty()) {
+                Trade trade = null;
+                try {
+                    trade = queue.take();
+                    //trade = queue.poll();
+                    Instrument instrument = allInstrumentMap.get(trade.getInstrumentId());
+                    if(instrument == null)
+                        throw new RuntimeException("Failed to report trade Id" +trade.getId());
+                    double unitValue = api.valueInstrument(instrument);
+                    String name = instrument.getName();
+                    double value = unitValue * trade.getQuantity();
+                    api.reportTradeValue(trade.getId(), name, value);
+                } catch (Throwable t) {
+                    api.reportErrorTrade(trade.getId());
                 }
+
             }
+            api.allTradesReported();
+            publisherNotCompleted.set(true);
+
+
         }).start();
     }
 
     private String getName(List<Instrument> allInstruments, Trade x) {
         return allInstruments.stream()
-                .filter(inst -> inst.getId() == x.getId())
+                //.filter(inst -> inst.getId() == x.getId())
+                .filter(inst -> inst.getId() == x.getInstrumentId())
+
                 .map(inst -> inst.getName())
                 .collect(Collectors.toList()).get(0);
     }
